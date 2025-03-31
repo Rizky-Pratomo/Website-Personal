0ee95e38bff3961f9d6d4995b66c5824e1ef40fc
Rizky-Pratomo <kyprtm20@gmail.com>
2025-03-31 18:34:13 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,23 +2,22 @@ document.addEventListener("DOMContentLoaded", function () {
     const toggleDarkMode = document.getElementById("toggleDarkMode");
     const body = document.body;
     
-   // Cek status mode gelap di LocalStorage
-   if (localStorage.getItem("dark-mode") === "enabled") {
-    body.classList.add("dark-mode");
-    toggleDarkMode.innerText = "Light Mode ☀️"; // Ikon matahari untuk mode terang
-}
-
-// Event listener untuk tombol Dark Mode
-toggleDarkMode.addEventListener("click", function () {
-    body.classList.toggle("dark-mode");
-
-    // Simpan status Dark Mode ke LocalStorage
-    if (body.classList.contains("dark-mode")) {
-        localStorage.setItem("dark-mode", "enabled");
-        toggleDarkMode.innerText = "Light Mode☀️"; // Mode terang
-    } else {
-        localStorage.setItem("dark-mode", "disabled");
-        toggleDarkMode.innerText = "Dark Mode 🌙"; // Mode gelap
+    // Cek status mode gelap di LocalStorage
+    if (localStorage.getItem("dark-mode") === "enabled") {
+        body.classList.add("dark-mode");
+        toggleDarkMode.innerText = "Light Mode ☀️"; 
     }
-});
+
+    // Event listener untuk tombol Dark Mode
+    toggleDarkMode.addEventListener("click", function () {
+        body.classList.toggle("dark-mode");
+
+        if (body.classList.contains("dark-mode")) {
+            localStorage.setItem("dark-mode", "enabled");
+            toggleDarkMode.innerText = "Light Mode ☀️";
+        } else {
+            localStorage.setItem("dark-mode", "disabled");
+            toggleDarkMode.innerText = "Dark Mode 🌙";
+        }
+    });
 });
